2a6f0a7dbd8b19d8eab869f676824f11d3224889
Kartik Shah <kartikshah@Kartiks-iMac.local>
2018-12-04 17:27:25 +0530

    added log4j

---
+++ src/main/java/com/kartiks/Abc.java
@@ -0,0 +1,12 @@
+package com.kartiks;
+
+import java.util.logging.LogManager;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LogManager.getLogManager();
+	}
+
+}

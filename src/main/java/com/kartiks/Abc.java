70f2932f6af64b457817ff3fff36ded740258782
Kartik Shah <kartikshah@Kartiks-iMac.local>
2018-12-06 15:49:31 +0530

    proper url for spring rest, log4jlogger, log4j with proper comments

--- src/main/java/com/kartiks/Abc.java
+++ src/main/java/com/kartiks/Abc.java
@@ -2,11 +2,15 @@ package com.kartiks;
 
 import java.util.logging.LogManager;
 
+import org.apache.log4j.Logger;
+
 public class Abc {
+	
+	static Logger logger = Logger.getLogger(Abc.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LogManager.getLogManager();
+		
 	}
 
 }
